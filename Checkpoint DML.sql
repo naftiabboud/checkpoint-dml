d33641543f5e90572a959905cf268c307797245e
naftiabboud <83706969+naftiabboud@users.noreply.github.com>
2021-08-25 01:11:26 +0200

    Add files via upload

---
+++ Checkpoint DML.sql
@@ -0,0 +1,14 @@
+
+
+
+INSERT INTO Product VALUES ('P01','Sumsung Galaxy S20','Smartphone',3299);
+
+INSERT INTO Product VALUES ('P02','ASUS Notebook','PC',4599);
+
+INSERT INTO Customer VALUES ('C01','Ali',71321009);
+
+INSERT INTO Customer VALUES ('C02','ASMA',71345823);
+
+INSERT INTO Orders VALUES (Customer_id,Product_id,quantity,total_amount)VALUES('C01','P02',2,9198);
+
+INSERT INTO Orders VALUES ('C02','P01',28/05/2020,1,3299)
